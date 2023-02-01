ead3ba5adde8d7d28ccfe3efd7cb95cc6a243305
Crowdbotics <engineering@crowdbotics.com>
2023-02-01 06:06:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "skira-dev-60589",
+  url: "https://skira-dev-60589.botics.co",
+  api: "https://skira-dev-60589.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
